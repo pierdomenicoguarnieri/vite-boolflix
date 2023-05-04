43ffe4fb74a390b6ae09e3cdfa26f72ce2b2f6e7
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-04 17:22:49 +0200

    add: header styling and logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,18 @@ export default {
 </script>
 
 <template>
-  
+  <header>
+    <img src="img/logo-boolflix.png" alt="">
+  </header>
 </template>
 
 <style lang="scss" scoped>
-
+  header{
+    height: 80px;
+    background-color: #000;
+    box-shadow: 10px 0 10px rgba(#000, 0.8);
+    img{
+      height: 100%;
+    }
+  }
 </style>
