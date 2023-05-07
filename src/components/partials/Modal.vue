695659f7885975dacc897e0023150e515c500532
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-07 23:54:59 +0200

    add: modal header

--- src/components/partials/Modal.vue
+++ src/components/partials/Modal.vue
@@ -31,6 +31,20 @@ export default {
   <div class="pg-modal-wrapper d-flex align-items-center justify-content-center overflow-scroll" v-if="store.showModal" @click="store.showModal = !store.showModal">
     <Loading v-if="store.isLoading"/>
     <div class="pg-modal overflow-hidden rounded-2 bg-dark text-white mb-2" v-else @click.stop="store.showModal = true">
+      <div class="pg-modal-header position-relative">
+        <div class="pg-modal-header-infos d-flex justify-content-between w-100 h-100 position-absolute p-3 top-0 left-0">
+          <div class="pg-title-container d-flex flex-column">
+            <h1 v-show="title != null">{{title}}</h1>
+            <h1 v-show="titleSerie!= null">{{titleSerie}}</h1>
+            <h2 v-show="translatedTitle != null">{{translatedTitle}}</h2>
+            <h2 v-show="translatedTitleSerie != null">{{translatedTitleSerie}}</h2>
+          </div>
+          <div class="btn-container">
+            <button type="button" class="btn btn-dark rounded-circle" @click.stop="store.showModal = !store.showModal"><i class="fa-solid fa-x"></i></button>
+          </div>
+        </div>
+        <img class="position-absolute top-0 left-0" :src="`https://image.tmdb.org/t/p/original${image}`" :alt="title">
+      </div>
       </div>
     </div>
   </div>
@@ -50,6 +64,19 @@ export default {
     top: 0;
     margin-top: 80px;
     width: 60%;
+    .pg-modal-header{
+      height: 400px;
+      overflow: hidden;
+      .pg-modal-header-infos{
+        z-index: 2;
+        background: rgb(0,0,0);
+        background: linear-gradient(90deg, rgba(0,0,0,1) 15%, rgba(255,255,255,0) 75%);
+      }
+      img{
+        z-index: 1;
+        width: 100%;
+      }
+    }
   }
 }
 </style>
