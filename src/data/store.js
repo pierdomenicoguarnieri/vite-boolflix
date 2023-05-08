cc53b8e02875f6e003a53fa8f29c280f30c87e65
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-08 21:49:32 +0200

    add: baseUrl and results array

--- src/data/store.js
+++ src/data/store.js
@@ -1,21 +1,16 @@
 import {reactive} from "vue";
 
 export const store = reactive({
-  apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=61bccd436e95e107643dd33da21f2885&language=it-IT",
-  apiUrlTv: "https://api.themoviedb.org/3/search/tv?api_key=61bccd436e95e107643dd33da21f2885&language=it-IT",
-  apiUrlPopular: "https://api.themoviedb.org/3/movie/popular?api_key=61bccd436e95e107643dd33da21f2885&language=it-IT",
-  apiUrlPopularSeries: "https://api.themoviedb.org/3/tv/top_rated?api_key=61bccd436e95e107643dd33da21f2885&language=it-IT",
-  apiUrlTopRated: "https://api.themoviedb.org/3/movie/top_rated?api_key=61bccd436e95e107643dd33da21f2885&language=it-IT",
-  resultMoviesArray: [],
-  resultSeriesArray: [],
-  resultPopularArray: [],
-  resultPopularSeriesArray: [],
+  baseUrl: "https://api.themoviedb.org/3/",
+  api_key: "61bccd436e95e107643dd33da21f2885",
+  language: "it-IT",
+  results: [],
   resultDetails: [],
   resultCredits: [],
-  resultTopRated: [],
   movieTitleToSearch: "",
   showModal: false,
   idInfos: 0,
   type: "",
-  isLoading: true
+  isLoading: true,
+  search: false,
 })
